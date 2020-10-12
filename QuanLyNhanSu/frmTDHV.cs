000a5f13b2f1acbd07dfc4f4bc0570800d2f8a70
0962769565 <74644355+0962769565@users.noreply.github.com>
2020-10-12 23:09:23 +0700

    Hoan thien module Trinh do hoc van

--- QuanLyNhanSu/frmTDHV.cs
+++ QuanLyNhanSu/frmTDHV.cs
@@ -64,7 +64,9 @@ namespace QuanLyNhanSu
         }
         private void clearData()
         {
-            
+            txtMaTDHV.Text = "";
+            txtTenTDHV.Text = "";
+            txtCN.Text = "";
         }
         private void HienThi()
         {
@@ -87,27 +89,77 @@ namespace QuanLyNhanSu
 
         private void btnSua_Click(object sender, EventArgs e)
         {
-            
+            fluu = 1;
+            DisEnl(true);
+            txtMaTDHV.Enabled = false;
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
         {
-            
+            if (MessageBox.Show("Bạn có chắc chắn muốn xóa không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    bus.XoaTDHV(txtMaTDHV.Text);
+                    MessageBox.Show("Xóa thành công!");
+                    clearData();
+                    DisEnl(false);
+                    HienThi();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi" + ex.Message);
+                }
+            }
         }
 
         private void btnLuu_Click(object sender, EventArgs e)
         {
-            
+            if (txtTenTDHV.Text=="" || txtCN.Text=="")
+            {
+                MessageBox.Show("Bạn chưa nhập đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            obj.MaTDHV = txtMaTDHV.Text;
+            obj.TenTrinhDo = txtTenTDHV.Text;
+            obj.ChuyenNganh = txtCN.Text;
+            if (fluu == 0)
+            {
+                bus.ThemTDHV(obj);
+                MessageBox.Show("Thêm thành công!");
+                HienThi();
+                clearData();
+                DisEnl(false);
+                fluu = 1;
+            }
+            else
+            {
+                bus.SuaTDHV(obj);
+                MessageBox.Show("Sửa Thành Công ! ");
+                HienThi();
+                clearData();
+                DisEnl(false);
+            }
         }
 
         private void dgvTrinhDoHocVan_Click(object sender, EventArgs e)
         {
-            
+            if (fluu == 0)
+            {
+                txtTenTDHV.Text = Convert.ToString(dgvTrinhDoHocVan.CurrentRow.Cells["TenTDHV"].Value);
+                txtCN.Text = Convert.ToString(dgvTrinhDoHocVan.CurrentRow.Cells["ChuyenNganh"].Value);
+            }
+            else
+            {
+                txtMaTDHV.Text = Convert.ToString(dgvTrinhDoHocVan.CurrentRow.Cells["MaTDHV"].Value);
+                txtTenTDHV.Text = Convert.ToString(dgvTrinhDoHocVan.CurrentRow.Cells["TenTDHV"].Value);
+                txtCN.Text = Convert.ToString(dgvTrinhDoHocVan.CurrentRow.Cells["ChuyenNganh"].Value);
+            }
         }
 
         private void dgvTrinhDoHocVan_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
         {
-           
+           dgvTrinhDoHocVan.Rows[e.RowIndex].Cells["STT"].Value = e.RowIndex + 1;
         }
     }
 }
